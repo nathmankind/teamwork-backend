a4fb3403818ad25f1499e285a394cc52acc22d13
Nathaniel Makinde <nathmankind@gmail.com>
2021-01-06 04:10:30 +0100

    completed gif implementation and comments

--- routes/article.js
+++ routes/article.js
@@ -4,12 +4,14 @@ const {
   getAllArticles,
   updateArticles,
   deleteArticle,
+  getOneArticle,
 } = require("../app/controllers/article");
 const verifyToken = require("../app/middleware/verifyAuth");
 const router = express.Router();
 
 router.get("/articles", verifyToken, getAllArticles);
 router.post("/articles/add", verifyToken, createArticle);
+router.get("/articles/:id", verifyToken, getOneArticle);
 router.put("/articles/:id", verifyToken, updateArticles);
 router.delete("/articles/:id", verifyToken, deleteArticle);
 
